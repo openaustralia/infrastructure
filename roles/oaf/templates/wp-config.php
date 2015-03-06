86415cc765b87d1dbee9a889c29eb73e801df52b
Matthew Landauer <matthew@oaf.org.au>
2015-03-06 16:52:07 +1100

    Add wordpress configuration file

--- roles/oaf/templates/wp-config.php
+++ roles/oaf/templates/wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -42,14 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         '{{ auth_key }}');
+define('SECURE_AUTH_KEY',  '{{ secure_auth_key }}');
+define('LOGGED_IN_KEY',    '{{ logged_in_key }}');
+define('NONCE_KEY',        '{{ nonce_key }}');
+define('AUTH_SALT',        '{{ auth_salt }}');
+define('SECURE_AUTH_SALT', '{{ secure_auth_salt }}');
+define('LOGGED_IN_SALT',   '{{ logged_in_salt }}');
+define('NONCE_SALT',       '{{ nonce_salt }}');
 
 /**#@-*/
 
