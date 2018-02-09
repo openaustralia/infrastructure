4d870ed9efdcde791080bdd627f374c54bc83849
Matthew Landauer <mlandauer@gmail.com>
2018-02-10 07:16:44 +1100

    Get the AMI by name and owner (canonincal)

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -10,11 +10,25 @@ provider "aws" {
   region     = "${var.ec2_region}"
 }
 
-# TODO: Don't hardcode ami but rather look it up
+# Get the AMI for Ubuntu 16.04. Lock it to a specific version so that we don't
+# keep re-provisioning the servers when the AMI gets updated
+data "aws_ami" "ubuntu" {
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180205"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  # Canonical
+  owners = ["099720109477"]
+}
 
 resource "aws_instance" "theyvoteforyou" {
-  # Ubuntu 16.04 in Sydney
-  ami =  "ami-974eb0f5"
+  ami =  "${data.aws_ami.ubuntu.id}"
   # t2.micro doesn't give us enough memory
   instance_type = "t2.small"
   key_name = "test"
