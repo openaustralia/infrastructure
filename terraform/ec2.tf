c66f96bf91a732d66483ea8761cf4c977aa4b07a
Matthew Landauer <mlandauer@gmail.com>
2018-02-09 11:21:28 +1100

    First go at using terraform with ec2

---
+++ terraform/ec2.tf
@@ -0,0 +1,49 @@
+# An experiment in setting up the OAF infrastructure on EC2 using terraform
+# (We're still using Ansible for configuring the servers themselves and
+# the normal application deployment is still done with capistrano)
+
+# Configure the AWS Provider
+provider "aws" {
+  version = "~> 1.8"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.ec2_region}"
+}
+
+resource "aws_instance" "theyvoteforyou" {
+  # Ubuntu 16.04 in Sydney
+  ami =  "ami-974eb0f5"
+  # t2.micro doesn't give us enough memory
+  instance_type = "t2.small"
+  key_name = "test"
+  tags {
+    Name = "theyvoteforyou"
+  }
+  vpc_security_group_ids = ["${aws_security_group.theyvoteforyou.id}"]
+}
+
+resource "aws_security_group" "theyvoteforyou" {
+  name        = "theyvoteforyou"
+  description = "theyvoteforyou security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+# Note that currently there is duplication between these values and those stored
+# and used in ansible
+# TODO: Remove duplication
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "ec2_region" {
+  # Sydney
+  default = "ap-southeast-2"
+}
