9ab6113779b69dffba4dd89e31369c0abaa0e219
Matthew Landauer <mlandauer@gmail.com>
2018-02-09 12:07:09 +1100

    Move stuff to its own file

---
+++ terraform/backend.tf
@@ -0,0 +1,19 @@
+# We're storing the terraform state in S3
+terraform {
+  backend "s3" {
+    bucket = "oaf-terraform-state"
+    key    = "terraform-state"
+    region =  "ap-southeast-2"
+    encrypt = true
+  }
+}
+
+# The S3 bucket for storing terraform state
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "oaf-terraform-state"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+}
--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,26 +2,6 @@
 # (We're still using Ansible for configuring the servers themselves and
 # the normal application deployment is still done with capistrano)
 
-# We're storing the terraform state in S3
-terraform {
-  backend "s3" {
-    bucket = "oaf-terraform-state"
-    key    = "terraform-state"
-    region =  "ap-southeast-2"
-    encrypt = true
-  }
-}
-
-# The S3 bucket for storing terraform state
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "oaf-terraform-state"
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-}
-
 # Configure the AWS Provider
 provider "aws" {
   version = "~> 1.8"
