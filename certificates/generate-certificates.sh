8cc97046cd07e234eca0bd0a37eab7eaa6d565ed
Matthew Landauer <mlandauer@gmail.com>
2018-02-11 17:15:07 +1100

    Generate certs for test.theyvoteforyou.org.au.dev

--- certificates/generate-certificates.sh
+++ certificates/generate-certificates.sh
@@ -27,18 +27,32 @@ fi
 echo "Generating private key for theyvoteforyou.org.au.dev..."
 openssl genrsa -out theyvoteforyou.org.au.dev.key 2048
 
+echo "Generating private key for test.theyvoteforyou.org.au.dev..."
+openssl genrsa -out test.theyvoteforyou.org.au.dev.key 2048
+
 # Generate a CSR (a request to the CA to sign a certificate)
 echo "Generating a request to sign certificate for theyvoteforyou.org.au.dev..."
 openssl req -new -key theyvoteforyou.org.au.dev.key -out theyvoteforyou.org.au.dev.csr \
   -subj '/C=AU/CN=theyvoteforyou.org.au.dev'
+
+# Generate a CSR (a request to the CA to sign a certificate)
+echo "Generating a request to sign certificate for test.theyvoteforyou.org.au.dev..."
+openssl req -new -key test.theyvoteforyou.org.au.dev.key -out test.theyvoteforyou.org.au.dev.csr \
+  -subj '/C=AU/CN=test.theyvoteforyou.org.au.dev'
 
 echo "Generate certificate for theyvoteforyou.org.au.dev..."
 openssl x509 -req -in theyvoteforyou.org.au.dev.csr -CA myCA.pem -CAkey myCA.key \
   -CAcreateserial -out theyvoteforyou.org.au.dev.pem -days 1825 -sha256 \
   -extfile theyvoteforyou.org.au.dev.ext -passin pass:abcd
 
+echo "Generate certificate for test.theyvoteforyou.org.au.dev..."
+openssl x509 -req -in test.theyvoteforyou.org.au.dev.csr -CA myCA.pem -CAkey myCA.key \
+  -CAcreateserial -out test.theyvoteforyou.org.au.dev.pem -days 1825 -sha256 \
+  -extfile test.theyvoteforyou.org.au.dev.ext -passin pass:abcd
+
 # Remove intermediate files
-rm theyvoteforyou.org.au.dev.csr myCA.srl
+rm theyvoteforyou.org.au.dev.csr test.theyvoteforyou.org.au.dev.csr myCA.srl
 
 # Move certificate into the right place
 mv theyvoteforyou.org.au.dev.key theyvoteforyou.org.au.dev.pem ../roles/internal/theyvoteforyou/files
+mv test.theyvoteforyou.org.au.dev.key test.theyvoteforyou.org.au.dev.pem ../roles/internal/theyvoteforyou/files
