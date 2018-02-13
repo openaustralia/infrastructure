904ce20c830eef17a59c419fc23141e9ef4da660
Matthew Landauer <mlandauer@gmail.com>
2018-02-13 11:29:27 +1100

    Order changes only

--- terraform/theyvoteforyou-dns.tf
+++ terraform/theyvoteforyou-dns.tf
@@ -9,36 +9,40 @@ provider "dme" {
 # Create an A record
 resource "dme_record" "ec2" {
   domainid    = "${var.theyvoteforyou_dme_domainid}"
+  ttl         = "${var.theyvoteforyou_dme_ttl}"
+  gtdLocation = "DEFAULT"
+
   type        = "A"
   name        = "ec2"
   value       = "${aws_eip.theyvoteforyou.public_ip}"
-  ttl         = "${var.theyvoteforyou_dme_ttl}"
-  gtdLocation = "DEFAULT"
 }
 
 resource "dme_record" "test_ec2" {
   domainid    = "${var.theyvoteforyou_dme_domainid}"
+  ttl         = "${var.theyvoteforyou_dme_ttl}"
+  gtdLocation = "DEFAULT"
+
   type        = "CNAME"
   name        = "test.ec2"
   value       = "ec2"
-  ttl         = "${var.theyvoteforyou_dme_ttl}"
-  gtdLocation = "DEFAULT"
 }
 
 resource "dme_record" "www_ec2" {
   domainid    = "${var.theyvoteforyou_dme_domainid}"
+  ttl         = "${var.theyvoteforyou_dme_ttl}"
+  gtdLocation = "DEFAULT"
+
   type        = "CNAME"
   name        = "www.ec2"
   value       = "ec2"
-  ttl         = "${var.theyvoteforyou_dme_ttl}"
-  gtdLocation = "DEFAULT"
 }
 
 resource "dme_record" "www_test_ec2" {
   domainid    = "${var.theyvoteforyou_dme_domainid}"
+  ttl         = "${var.theyvoteforyou_dme_ttl}"
+  gtdLocation = "DEFAULT"
+
   type        = "CNAME"
   name        = "www.test.ec2"
   value       = "ec2"
-  ttl         = "${var.theyvoteforyou_dme_ttl}"
-  gtdLocation = "DEFAULT"
 }
