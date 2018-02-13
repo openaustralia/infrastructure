490ae816cb3396d6e8b75260c9bde82d4ea87355
Matthew Landauer <mlandauer@gmail.com>
2018-02-14 04:44:28 +1100

    Use bash loops to simplify generation of multiple certificates

--- certificates/generate-certificates.sh
+++ certificates/generate-certificates.sh
@@ -3,6 +3,8 @@
 # Generates certificates for local development. A single certificate, that
 # is unique to you, is added to your browser
 
+domains=( "theyvoteforyou.org.au.dev" "test.theyvoteforyou.org.au.dev" )
+
 # Generates a private key with passphrase "abcd" (but only if it doesn't already exist)
 if [ ! -f myCA.key ]; then
   echo "Generating private key for root certificate..."
@@ -24,34 +26,24 @@ if [ ! -f myCA.pem ]; then
   echo "**********************************************************************************************"
 fi
 
-echo "Generating private key for theyvoteforyou.org.au.dev..."
-openssl genrsa -out theyvoteforyou.org.au.dev.key 2048
+for domain in "${domains[@]}"
+do
+  echo "Generating private key for $domain..."
+  openssl genrsa -out $domain.key 2048
 
-echo "Generating private key for test.theyvoteforyou.org.au.dev..."
-openssl genrsa -out test.theyvoteforyou.org.au.dev.key 2048
+  # Generate a CSR (a request to the CA to sign a certificate)
+  echo "Generating a request to sign certificate for $domain..."
+  openssl req -new -key $domain.key -out $domain.csr \
+    -subj "/C=AU/CN=$domain"
 
-# Generate a CSR (a request to the CA to sign a certificate)
-echo "Generating a request to sign certificate for theyvoteforyou.org.au.dev..."
-openssl req -new -key theyvoteforyou.org.au.dev.key -out theyvoteforyou.org.au.dev.csr \
-  -subj '/C=AU/CN=theyvoteforyou.org.au.dev'
+  echo "Generate certificate for $domain..."
+  openssl x509 -req -in $domain.csr -CA myCA.pem -CAkey myCA.key \
+    -CAcreateserial -out $domain.pem -days 1825 -sha256 \
+    -extfile $domain.ext -passin pass:abcd
 
-# Generate a CSR (a request to the CA to sign a certificate)
-echo "Generating a request to sign certificate for test.theyvoteforyou.org.au.dev..."
-openssl req -new -key test.theyvoteforyou.org.au.dev.key -out test.theyvoteforyou.org.au.dev.csr \
-  -subj '/C=AU/CN=test.theyvoteforyou.org.au.dev'
-
-echo "Generate certificate for theyvoteforyou.org.au.dev..."
-openssl x509 -req -in theyvoteforyou.org.au.dev.csr -CA myCA.pem -CAkey myCA.key \
-  -CAcreateserial -out theyvoteforyou.org.au.dev.pem -days 1825 -sha256 \
-  -extfile theyvoteforyou.org.au.dev.ext -passin pass:abcd
-
-echo "Generate certificate for test.theyvoteforyou.org.au.dev..."
-openssl x509 -req -in test.theyvoteforyou.org.au.dev.csr -CA myCA.pem -CAkey myCA.key \
-  -CAcreateserial -out test.theyvoteforyou.org.au.dev.pem -days 1825 -sha256 \
-  -extfile test.theyvoteforyou.org.au.dev.ext -passin pass:abcd
-
-# Remove intermediate files
-rm theyvoteforyou.org.au.dev.csr test.theyvoteforyou.org.au.dev.csr myCA.srl
+  # Remove intermediate files
+  rm $domain.csr myCA.srl
+done
 
 # Move certificate into the right place
 mv theyvoteforyou.org.au.dev.key theyvoteforyou.org.au.dev.pem ../roles/internal/theyvoteforyou/files
