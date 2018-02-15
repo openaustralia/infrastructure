20dfec0b14762942325954895a453938e281c630
Matthew Landauer <mlandauer@gmail.com>
2018-02-16 10:27:03 +1100

    Preparing planningalerts for production

--- terraform/planningalerts.tf
+++ terraform/planningalerts.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "planningalerts" {
   ami =  "${data.aws_ami.ubuntu.id}"
-  # TODO: Bump this up for production
-  instance_type = "t2.small"
+  # A quick look at newrelic is showing PlanningAlerts on kedumba
+  # using about 1.5GB. A medium instance gives us 4GB
+  instance_type = "t2.medium"
   key_name = "test"
   tags {
     Name = "planningalerts"
   }
   security_groups = ["${aws_security_group.webserver.name}"]
-  # TODO: Reenable this for production
-  # disable_api_termination = true
+  disable_api_termination = true
 }
 
 resource "aws_eip" "planningalerts" {
