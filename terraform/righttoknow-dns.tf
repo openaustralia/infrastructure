9f7c38699d6504d6a0a96d102a34258d6708445c
Matthew Landauer <mlandauer@gmail.com>
2018-02-19 09:43:53 +1100

    Move righttoknow.org.au dns to cloudflare

---
+++ terraform/righttoknow-dns.tf
@@ -0,0 +1,89 @@
+# A records
+resource "cloudflare_record" "rtk_root" {
+  domain = "righttoknow.org.au"
+  name   = "righttoknow.org.au"
+  type   = "A"
+  # TODO: Get this value from elsewhere
+  value  = "54.79.92.207"
+}
+
+# CNAME records
+resource "cloudflare_record" "rtk_test" {
+  domain = "righttoknow.org.au"
+  name   = "test.righttoknow.org.au"
+  type   = "CNAME"
+  value  = "righttoknow.org.au"
+}
+
+resource "cloudflare_record" "rtk_www" {
+  domain = "righttoknow.org.au"
+  name   = "www.righttoknow.org.au"
+  type   = "CNAME"
+  value  = "righttoknow.org.au"
+}
+
+# MX records
+
+resource "cloudflare_record" "rtk_mx1" {
+  domain   = "righttoknow.org.au"
+  name     = "righttoknow.org.au"
+  type     = "MX"
+  priority = 10
+  value    = "aspmx.l.google.com"
+}
+
+resource "cloudflare_record" "rtk_mx2" {
+  domain   = "righttoknow.org.au"
+  name     = "righttoknow.org.au"
+  type     = "MX"
+  priority = 20
+  value    = "alt1.aspmx.l.google.com"
+}
+
+resource "cloudflare_record" "rtk_mx3" {
+  domain   = "righttoknow.org.au"
+  name     = "righttoknow.org.au"
+  type     = "MX"
+  priority = 20
+  value    = "alt2.aspmx.l.google.com"
+}
+
+resource "cloudflare_record" "rtk_mx4" {
+  domain   = "righttoknow.org.au"
+  name     = "righttoknow.org.au"
+  type     = "MX"
+  priority = 30
+  value    = "aspmx2.googlemail.com"
+}
+
+resource "cloudflare_record" "rtk_mx5" {
+  domain   = "righttoknow.org.au"
+  name     = "righttoknow.org.au"
+  type     = "MX"
+  priority = 30
+  value    = "aspmx3.googlemail.com"
+}
+
+# TODO Check how this record is being used
+resource "cloudflare_record" "rtk_server" {
+  domain   = "righttoknow.org.au"
+  name     = "server.righttoknow.org.au"
+  type     = "MX"
+  priority = 10
+  value    = "righttoknow.org.au"
+}
+
+# TXT records
+resource "cloudflare_record" "rtk_spf" {
+  domain = "righttoknow.org.au"
+  name   = "righttoknow.org.au"
+  type   = "TXT"
+  value  = "v=spf1 a include:_spf.google.com ~all"
+}
+
+resource "cloudflare_record" "rtk_google_site_verification" {
+  domain = "righttoknow.org.au"
+  name   = "righttoknow.org.au"
+  type   = "TXT"
+  value  = "google-site-verification=CPi5guMn0IiJjjYusPOb3ziStX_vKDqyk-cs0cLZvHc"
+}
