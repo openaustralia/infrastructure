2dcf4465b6b57ba2b1da572386bfc773060a88e9
Matthew Landauer <mlandauer@gmail.com>
2018-02-19 09:06:12 +1100

    Add octopus ec2 instance setup to terraform

--- terraform/legacy.tf
+++ terraform/legacy.tf
@@ -10,3 +10,16 @@ resource "aws_instance" "jamison" {
 resource "aws_eip" "jamison" {
   instance = "${aws_instance.jamison.id}"
 }
+
+resource "aws_instance" "octopus" {
+  ami = "ami-33ab5251"
+  instance_type = "t2.large"
+  tags {
+    Name = "octopus"
+  }
+  security_groups = ["${aws_security_group.octopus.name}"]
+}
+
+resource "aws_eip" "octopus" {
+  instance = "${aws_instance.octopus.id}"
+}
--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -144,3 +144,46 @@ resource "aws_security_group" "jamison" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "octopus" {
+  # TODO Change this
+  description = "for main instance"
+  
+  # TODO: Remove this
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ssh is running on port 2506
+  ingress {
+    from_port   = 2506
+    to_port     = 2506
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow pings from hosts on the internet
+  ingress {
+    protocol = "icmp"
+    from_port = 8
+    to_port = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
