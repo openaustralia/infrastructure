48078d186927e39ac6b4d4a530d98643cafdaaa7
Matthew Landauer <mlandauer@gmail.com>
2018-03-02 06:52:12 +1100

    Create a 10GB block device for data and attach it to openaustralia

--- terraform/openaustralia.tf
+++ terraform/openaustralia.tf
@@ -17,3 +17,25 @@ resource "aws_eip" "openaustralia" {
     Name = "openaustralia"
   }
 }
+
+# We'll create a seperate EBS volume for all the application
+# data that can not be regenerated. e.g. parliamentary XML,
+# register of members interests scans, etc..
+
+resource "aws_ebs_volume" "openaustralia_data" {
+    availability_zone = "${aws_instance.openaustralia.availability_zone}"
+    # 10 Gb is an educated guess based on seeing how much space is taken up
+    # on kedumba.
+    # TODO: This might need to be increased for production
+    size = 10
+    type = "gp2"
+    tags {
+        Name = "openaustralia_data"
+    }
+}
+
+resource "aws_volume_attachment" "openaustralia_data" {
+  device_name = "/dev/sdh"
+  volume_id   = "${aws_ebs_volume.openaustralia_data.id}"
+  instance_id = "${aws_instance.openaustralia.id}"
+}
