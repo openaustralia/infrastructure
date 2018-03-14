405c2e1e7b2b82b53fe22be586e221f3ddaeb891
Matthew Landauer <mlandauer@gmail.com>
2018-03-14 12:29:11 +1100

    Create righttoknow instance

---
+++ terraform/righttoknow.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "righttoknow" {
+  ami =  "${data.aws_ami.ubuntu.id}"
+  # TODO: For production probably will need to be t2.medium at a minimum
+  instance_type = "t2.small"
+  key_name = "test"
+  tags {
+    Name = "righttoknow"
+  }
+  security_groups = ["${aws_security_group.webserver.name}"]
+  # TODO: For production set disable_api_termination to true
+  disable_api_termination = false
+}
+
+resource "aws_eip" "righttoknow" {
+  instance = "${aws_instance.righttoknow.id}"
+  tags {
+    Name = "righttoknow"
+  }
+}
