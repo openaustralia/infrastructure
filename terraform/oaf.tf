aabc3d7aa83a36aa501898f23edde71235c9e83e
Matthew Landauer <mlandauer@gmail.com>
2018-03-22 04:40:47 +1100

    Set up real server for oaf on ec2

---
+++ terraform/oaf.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "oaf" {
+  ami =  "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.small"
+  key_name = "test"
+  tags {
+    Name = "oaf"
+  }
+  security_groups = ["${aws_security_group.webserver.name}"]
+  # TODO: For production set to true
+  disable_api_termination = false
+}
+
+resource "aws_eip" "oaf" {
+  instance = "${aws_instance.oaf.id}"
+  tags {
+    Name = "oaf"
+  }
+}
