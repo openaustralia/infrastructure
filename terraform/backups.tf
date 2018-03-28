761d1ad60962e909eae34defe6629080bceea969
Matthew Landauer <mlandauer@gmail.com>
2018-03-28 12:56:51 +1100

    Setup user, policy and bucket for backups

---
+++ terraform/backups.tf
@@ -0,0 +1,45 @@
+resource "aws_iam_user" "oaf-backups" {
+  name = "oaf-backups"
+}
+
+resource "aws_iam_policy" "oaf-backups" {
+  name = "S3BucketAccessTo_oaf-backups"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListAllMyBuckets"
+            ],
+            "Resource": "arn:aws:s3:::*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket",
+                "s3:GetBucketLocation"
+            ],
+            "Resource": "arn:aws:s3:::oaf-backups"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:DeleteObject"
+            ],
+            "Resource": "arn:aws:s3:::oaf-backups/*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_s3_bucket" "oaf-backups" {
+  provider = "aws.us-east-1"
+  bucket = "oaf-backups"
+  region = "us-east-1"
+  acl = "private"
+}
