200969a7d30e5f27a3b1123ff752ea0bf0bbc3d7
James Polley <jp@jamezpolley.com>
2018-03-28 16:30:06 +1100

    Update DNS records for electionleaflets switch to ec2

--- terraform/electionleaflets-dns.tf
+++ terraform/electionleaflets-dns.tf
@@ -3,7 +3,7 @@ resource "cloudflare_record" "el_root" {
   domain = "electionleaflets.org.au"
   name   = "electionleaflets.org.au"
   type   = "A"
-  value  = "${aws_eip.kedumba.public_ip}"
+  value  = "${aws_eip.electionleaflets.public_ip}"
 }
 
 resource "cloudflare_record" "el_ec2" {
