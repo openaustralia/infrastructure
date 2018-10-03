7766ced0052c3a42d0b53dd22bfdc8076068cea7
Ben Fairless <ben@righttoknow.org.au>
2018-10-03 18:31:39 +0800

    RTK Front DNS

--- terraform/righttoknow-dns.tf
+++ terraform/righttoknow-dns.tf
@@ -92,3 +92,26 @@ resource "cloudflare_record" "rtk_google_site_verification" {
   type   = "TXT"
   value  = "google-site-verification=CPi5guMn0IiJjjYusPOb3ziStX_vKDqyk-cs0cLZvHc"
 }
+
+#Front DNS records
+resource "cloudflare_record" "oaf_rtk_front_mx" {
+  domain = "righttoknow.org.au"
+  name = "front-mail.righttoknow.org.au"
+  type = "MX"
+  priority = 100
+  value = "mx.sendgrid.net"
+}
+
+resource "cloudflare_record" "oaf_rtk_front_spf" {
+  domain = "righttoknow.org.au"
+  name   = "front-mail.righttoknow.org.au"
+  type   = "TXT"
+  value  = "v=spf1 a include:sendgrid.net ~all"
+}
+
+resource "cloudflare_record" "oaf_rtk_front_domainkey" {
+  domain = "righttoknow.org.au"
+  name   = "m1._domainkey.righttoknow.org.au"
+  type   = "TXT"
+  value  = "k=rsa; t=s; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC4PZZJiwMfMB/CuIZ9yAtNEGzfKzQ7WC7hfGg8UyavtYlDDBgSP6P1AiTBTMzTQbLChvf+Ef5CK46w+RwmgWpL38sxRwjahk45aQxoMOk2FJm7iHnP6zAGUnqAiL8iCdTjn5sp/txNf22bXrx3YS54ePBrfZQxOvkOvE24XZKXXwIDAQAB"
+}
