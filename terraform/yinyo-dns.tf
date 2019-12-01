56d650b94ed43427e48709b64fbf824e1cd230b3
Matthew Landauer <mlandauer@gmail.com>
2019-12-01 16:32:28 +1100

    Setup github pages for yinyo.io

---
+++ terraform/yinyo-dns.tf
@@ -0,0 +1,32 @@
+variable "yinyo_io_zone_id" {
+  default = "9734850222324fac212c52ba30615585"
+}
+
+# A records
+resource "cloudflare_record" "yinyo_root1" {
+  zone_id = var.yinyo_io_zone_id
+  name    = "yinyo.io"
+  type    = "A"
+  value   = "185.199.108.153"
+}
+
+resource "cloudflare_record" "yinyo_root2" {
+  zone_id = var.yinyo_io_zone_id
+  name    = "yinyo.io"
+  type    = "A"
+  value   = "185.199.109.153"
+}
+
+resource "cloudflare_record" "yinyo_root3" {
+  zone_id = var.yinyo_io_zone_id
+  name    = "yinyo.io"
+  type    = "A"
+  value   = "185.199.110.153"
+}
+
+resource "cloudflare_record" "yinyo_root4" {
+  zone_id = var.yinyo_io_zone_id
+  name    = "yinyo.io"
+  type    = "A"
+  value   = "185.199.111.153"
+}
