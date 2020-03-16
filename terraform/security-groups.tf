ec8c1cdff58c0c1511061e9f115d9b29a743bf3d
Matthew Landauer <mlandauer@gmail.com>
2020-03-16 12:27:04 +1100

    Remove old unused security groups

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -134,55 +134,6 @@ resource "aws_security_group" "postgresql" {
   }
 }
 
-# TODO: Remove this
-resource "aws_security_group" "jamison" {
-  name        = "jamison"
-  description = "launch-wizard-3 created 2018-02-19T05:58:53.596+11:00"
-
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  # TODO: Remove this once all database migrations from jamison are complete
-  ingress {
-    description     = "Allow database migration service access to database on jamison"
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [data.aws_security_group.default.id]
-  }
-
-  # Allow pings from hosts on the internet
-  ingress {
-    protocol         = "icmp"
-    from_port        = 8
-    to_port          = -1
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  # Allow everything going out
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
-
 resource "aws_security_group" "incoming_email" {
   name        = "incoming_email"
   description = "Allows incoming email"
@@ -190,73 +141,6 @@ resource "aws_security_group" "incoming_email" {
     from_port        = 25
     to_port          = 25
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
-
-# TODO: Remove this
-resource "aws_security_group" "kedumba" {
-  name        = "octopus"
-  description = "for main instance"
-
-  # ssh is running on port 2506
-  ingress {
-    from_port        = 2506
-    to_port          = 2506
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  # Allow incoming email (for righttoknow)
-  ingress {
-    from_port        = 25
-    to_port          = 25
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  # TODO: Remove this once all database migrations from kedumba are complete
-  ingress {
-    description     = "Allow database migration service access to database on kedumba"
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [data.aws_security_group.default.id]
-  }
-
-  # Allow pings from hosts on the internet
-  ingress {
-    protocol         = "icmp"
-    from_port        = 8
-    to_port          = -1
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  # Allow everything going out
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
