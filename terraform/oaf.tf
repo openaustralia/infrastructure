e8a686cdd1ed5b228c528bd690ad2f5fc7cff3f1
Matthew Landauer <mlandauer@gmail.com>
2020-03-16 12:46:26 +1100

    Make oaf.org.au server smaller because it's just really running wordpress now

--- terraform/oaf.tf
+++ terraform/oaf.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "oaf" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.medium"
+  instance_type = "t2.small"
   key_name      = "deployer_key"
   tags = {
     Name = "oaf"
