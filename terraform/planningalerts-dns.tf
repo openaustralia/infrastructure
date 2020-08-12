de0002391a28a9610803bb560154ad80028fb295
Matthew Landauer <mlandauer@gmail.com>
2020-08-12 12:33:46 +1000

    Send dmarc reports to free postmarkapp service for digests going to contact@oaf.org.au

--- terraform/planningalerts-dns.tf
+++ terraform/planningalerts-dns.tf
@@ -138,7 +138,7 @@ resource "cloudflare_record" "pa_dmarc" {
   zone_id = var.planningalerts_org_au_zone_id
   name = "_dmarc.planningalerts.org.au"
   type = "TXT"
-  value = "v=DMARC1; p=none; sp=none; rua=mailto:contact@planningalerts.org.au;"
+  value = "v=DMARC1; p=none; pct=100; rua=mailto:re+b1g0fn6boqu@dmarc.postmarkapp.com; sp=none; aspf=r;"
 }
 
 #Front DNS records
