67c9f293d010a86c39855642428d3fc131fc3725
Matthew Landauer <mlandauer@gmail.com>
2020-11-16 16:20:37 +1100

    At some point we bumped planningalerts up to xlarge. Don't remember why

--- terraform/planningalerts.tf
+++ terraform/planningalerts.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "planningalerts" {
   # After moving to sidekiq we seem to be needing some more memory.
   # So increased the instance type to t2.large.
   # TODO: It would be good to check if we can go smaller again
-  instance_type = "t2.large"
+  instance_type = "t2.xlarge"
   key_name      = "test"
   tags = {
     Name = "planningalerts"
