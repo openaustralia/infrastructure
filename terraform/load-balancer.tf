544777d1a29aefc34ec40a9adc970de69aa0b326
Matthew Landauer <mlandauer@gmail.com>
2021-07-19 15:49:46 +1000

    Add the first application load balancer listener

--- terraform/load-balancer.tf
+++ terraform/load-balancer.tf
@@ -12,3 +12,13 @@ resource "aws_lb" "main" {
   subnets = data.aws_subnet_ids.default.ids
 }
 
+resource "aws_lb_listener" "main-http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.planningalerts.arn
+  }
+}
