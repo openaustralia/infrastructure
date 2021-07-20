fe846c69e3dfc7ffb12532c5a1d65d26af6910ac
Matthew Landauer <mlandauer@gmail.com>
2021-07-21 04:55:20 +1000

    Redirect http on the load balancer to https

--- terraform/load-balancer.tf
+++ terraform/load-balancer.tf
@@ -19,8 +19,13 @@ resource "aws_lb_listener" "main-http" {
   protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.planningalerts.arn
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 
