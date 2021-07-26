59616f14fb74c0cc4ced98ee960350dcfcd53207
Matthew Landauer <mlandauer@gmail.com>
2021-07-26 11:12:49 +1000

    Add TODO

--- terraform/deployer-key.tf
+++ terraform/deployer-key.tf
@@ -2,6 +2,11 @@ data "external" "id_rsa" {
   program = ["./prepkey.sh"]
 }
 
+# TODO: Change instance keys for all instances to use this
+# We can't just change them all at once because it will recreate the instances
+# (VERY BAD). Instead we probably want to move over slowly as we upgrade or
+# rebuild instances
+
 # Make a key pair on AWS based on the first local key on your
 # development machine
 resource "aws_key_pair" "deployer" {
