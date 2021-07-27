4672d199d91f1afbd34de4dc65e6998b7af4e00d
Matthew Landauer <mlandauer@gmail.com>
2021-07-28 06:08:22 +1000

    Allow incoming ports 8000, 9000 for PA

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -179,6 +179,50 @@ resource "aws_security_group" "incoming_email" {
 resource "aws_security_group" "planningalerts" {
   name = "planningalerts"
   description = "Web servers for PlanningAlerts"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  # Production http port
+  ingress {
+    from_port        = 8000
+    to_port          = 8000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  # Staging http port
+  ingress {
+    from_port        = 9000
+    to_port          = 9000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  # Allow pings from hosts on the internet
+  ingress {
+    protocol         = "icmp"
+    from_port        = 8
+    to_port          = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  # Allow everything going out
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 resource "aws_security_group" "redis-planningalerts" {
