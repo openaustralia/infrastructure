cb8a08eb6fd772ab477729f22fb88dbd54a9d2f3
Matthew Landauer <mlandauer@gmail.com>
2022-02-09 14:02:39 +1100

    Rename resources

--- terraform/theyvoteforyou.tf
+++ terraform/theyvoteforyou.tf
@@ -1,7 +1,7 @@
 # (We're still using Ansible for configuring the servers themselves and
 # the normal application deployment is still done with capistrano)
 
-resource "aws_instance" "theyvoteforyou2" {
+resource "aws_instance" "theyvoteforyou" {
   ami = var.ubuntu_18_ami
 
   # t2.small was running at 100% cpu on the production load (due to the ruby web
@@ -10,16 +10,16 @@ resource "aws_instance" "theyvoteforyou2" {
   ebs_optimized = true
   key_name      = aws_key_pair.deployer.key_name
   tags = {
-    Name = "theyvoteforyou2"
+    Name = "theyvoteforyou"
   }
   security_groups         = [aws_security_group.webserver.name]
   disable_api_termination = true
   iam_instance_profile    = aws_iam_instance_profile.logging.name
 }
 
-resource "aws_eip" "theyvoteforyou2" {
-  instance = aws_instance.theyvoteforyou2.id
+resource "aws_eip" "theyvoteforyou" {
+  instance = aws_instance.theyvoteforyou.id
   tags = {
-    Name = "theyvoteforyou2"
+    Name = "theyvoteforyou"
   }
 }
