bcc20b289d8b5862c40622488b88a0ad9e18bd8d
Matthew Landauer <mlandauer@gmail.com>
2023-01-12 04:08:12 +1100

    Rename tag

--- terraform/planningalerts.tf
+++ terraform/planningalerts.tf
@@ -3,38 +3,6 @@ variable "availability_zones" {
   default = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
 }
 
-# resource "aws_instance" "planningalerts" {
-#   count = 2
-#   ami = var.ubuntu_16_ami
-
-#   # A quick look at newrelic is showing PlanningAlerts on kedumba
-#   # using about 1.5GB. A medium instance gives us 4GB
-#   # We t2.medium we were running out of memory when the scraping and emailing
-#   # setup happens at 12pm. There was also a bug which was causing multiple
-#   # instances of this to run at the same time until the memory was exhausted
-#   # and the server crashed. So, upped the instance size just to be on the
-#   # safe size.
-#   # After a couple of days of seeing the memory behaviour around 12pm
-#   # with the new instance size we realised we could in fact move back down
-#   # to the smaller t2.medium.
-#   # After moving to sidekiq we seem to be needing some more memory.
-#   # So increased the instance type to t2.large.
-#   # TODO: It would be good to check if we can go smaller again
-#   instance_type = "t3.medium"
-#   ebs_optimized = true
-#   key_name      = aws_key_pair.deployer.key_name
-#   tags = {
-#     Name = "web${count.index+1}.planningalerts"
-#   }
-#   security_groups         = [
-#     aws_security_group.planningalerts.name
-#   ]
-#   disable_api_termination = true
-#   iam_instance_profile    = aws_iam_instance_profile.logging.name
-
-#   availability_zone = var.availability_zones[count.index % 3]
-# }
-
 resource "aws_instance" "planningalerts" {
   count = 2
   ami = var.ubuntu_22_ami
@@ -43,7 +11,7 @@ resource "aws_instance" "planningalerts" {
   ebs_optimized = true
   key_name      = aws_key_pair.deployer.key_name
   tags = {
-    Name = "new.web${count.index+1}.planningalerts"
+    Name = "web${count.index+1}.planningalerts"
   }
   security_groups         = [
     aws_security_group.planningalerts.name
