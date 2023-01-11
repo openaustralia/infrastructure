dde374444b4a2d81e11bc716aa487ce3f5b2ca8f
Matthew Landauer <mlandauer@gmail.com>
2023-01-12 03:59:28 +1100

    Rename dns for new PA servers. Also reinstate crons

--- terraform/planningalerts-dns.tf
+++ terraform/planningalerts-dns.tf
@@ -4,18 +4,10 @@ variable "planningalerts_org_au_zone_id" {
 
 # A records
 
-# resource "cloudflare_record" "pa_web" {
-#   count = length(aws_eip.planningalerts)
-#   zone_id = var.planningalerts_org_au_zone_id
-#   name    = "web${count.index+1}.planningalerts.org.au"
-#   type    = "A"
-#   value   = aws_eip.planningalerts[count.index].public_ip
-# }
-
 resource "cloudflare_record" "pa_web" {
   count = length(aws_eip.new_planningalerts)
   zone_id = var.planningalerts_org_au_zone_id
-  name    = "new.web${count.index+1}.planningalerts.org.au"
+  name    = "web${count.index+1}.planningalerts.org.au"
   type    = "A"
   value   = aws_eip.new_planningalerts[count.index].public_ip
 }
