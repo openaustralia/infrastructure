ae38f2a6cf86ddfa2b732c1e170dcc15918df6e3
Matthew Landauer <mlandauer@gmail.com>
2023-01-12 03:24:52 +1100

    Remove original PA servers from load balancer

--- terraform/planningalerts.tf
+++ terraform/planningalerts.tf
@@ -139,17 +139,17 @@ resource "aws_lb_target_group" "planningalerts-staging" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "planningalerts-production" {
-  count = length(aws_instance.planningalerts)
-  target_group_arn = aws_lb_target_group.planningalerts-production.arn
-  target_id        = aws_instance.planningalerts[count.index].id
-}
+# resource "aws_lb_target_group_attachment" "planningalerts-production" {
+#   count = length(aws_instance.planningalerts)
+#   target_group_arn = aws_lb_target_group.planningalerts-production.arn
+#   target_id        = aws_instance.planningalerts[count.index].id
+# }
 
-resource "aws_lb_target_group_attachment" "planningalerts-staging" {
-  count = length(aws_instance.planningalerts)
-  target_group_arn = aws_lb_target_group.planningalerts-staging.arn
-  target_id        = aws_instance.planningalerts[count.index].id
-}
+# resource "aws_lb_target_group_attachment" "planningalerts-staging" {
+#   count = length(aws_instance.planningalerts)
+#   target_group_arn = aws_lb_target_group.planningalerts-staging.arn
+#   target_id        = aws_instance.planningalerts[count.index].id
+# }
 
 resource "aws_lb_target_group_attachment" "new_planningalerts-production" {
   count = length(aws_instance.new_planningalerts)
