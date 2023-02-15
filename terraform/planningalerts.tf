a4043728f7b6b007f18de882f1fe933bb4f9671b
Matthew Landauer <mlandauer@gmail.com>
2023-02-15 12:17:09 +1100

    Add aws secrets to ansible for planningalerts

--- terraform/planningalerts.tf
+++ terraform/planningalerts.tf
@@ -340,6 +340,14 @@ resource "aws_iam_access_key" "planningalerts_sitemaps_production" {
 }
 
 # These values are needed by ansible for planningalerts
+# They should be encrypted and put in group_vars/planningalerts.yml
+# Take the output of this command:
+# terraform output planningalerts_sitemaps_production_access_key_id
+# cd ..; ansible-vault encrypt_string --name aws_access_key_id "value from above" --encrypt-vault-id default
+# AND
+# terraform output planningalerts_sitemaps_production_secret_access_key
+# cd ..; ansible-vault encrypt_string --name aws_secret_access_key "value from above" --encrypt-vault-id default
+
 output "planningalerts_sitemaps_production_secret_access_key" {
   value = aws_iam_access_key.planningalerts_sitemaps_production.secret
 }
