8711fcbed8aecd356c36981ddf565d0523e2d02e
Matthew Landauer <mlandauer@gmail.com>
2023-04-09 07:00:26 +1000

    Remove deprecation warning

--- terraform/planningalerts-activestorage-s3/main.tf
+++ terraform/planningalerts-activestorage-s3/main.tf
@@ -1,11 +1,14 @@
 resource "aws_s3_bucket" "main" {
   bucket = "planningalerts-as-${var.env}"
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
+
