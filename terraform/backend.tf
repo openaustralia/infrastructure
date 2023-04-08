ceace8b783c30911f290216d05f5b4c44da230ef
Matthew Landauer <mlandauer@gmail.com>
2023-04-09 05:56:05 +1000

    Remove deprecation warnings

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -11,17 +11,27 @@ terraform {
 # The S3 bucket for storing terraform state
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "oaf-terraform-state"
-  acl    = "private"
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status     = "Enabled"
   }
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
+}
+
+resource "aws_s3_bucket_acl" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  acl = "private"
 }
