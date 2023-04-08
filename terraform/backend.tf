4e3a498945ece2bdb003298cc3d01b507e78e963
Matthew Landauer <mlandauer@gmail.com>
2023-04-09 06:07:34 +1000

    extra confidently block public access for one s3 bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -35,3 +35,12 @@ resource "aws_s3_bucket_acl" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
   acl = "private"
 }
+
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
