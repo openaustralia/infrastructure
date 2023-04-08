a3ee6ad77bcfa6e396993a658a33acf1e4b1580a
Matthew Landauer <mlandauer@gmail.com>
2023-04-09 06:26:10 +1000

    Remove deprecation warnings

--- terraform/backups.tf
+++ terraform/backups.tf
@@ -46,13 +46,22 @@ resource "aws_iam_user_policy_attachment" "oaf-backups" {
 resource "aws_s3_bucket" "oaf-backups" {
   provider = aws.us-east-1
   bucket   = "oaf-backups"
-  acl      = "private"
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_server_side_encryption_configuration" "oaf-backups" {
+  provider = aws.us-east-1
+  bucket = aws_s3_bucket.oaf-backups.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
+}
+
+resource "aws_s3_bucket_acl" "oaf-backups" {
+  provider = aws.us-east-1
+  bucket = aws_s3_bucket.oaf-backups.id
+
+  acl = "private"
 }
