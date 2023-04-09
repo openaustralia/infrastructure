787377b80325204fac7020694b12f86a498319bd
Matthew Landauer <mlandauer@gmail.com>
2023-04-10 09:41:01 +1000

    Make new s3 bucket extra private

--- terraform/planningalerts-activestorage-s3/main.tf
+++ terraform/planningalerts-activestorage-s3/main.tf
@@ -12,3 +12,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket                  = aws_s3_bucket.main.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
