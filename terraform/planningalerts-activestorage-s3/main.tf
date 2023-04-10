de627dd66d9873dd0ce6d09d8d8d2fd049b35e0f
Matthew Landauer <mlandauer@gmail.com>
2023-04-10 10:07:07 +1000

    Give new user permission on s3

--- terraform/planningalerts-activestorage-s3/main.tf
+++ terraform/planningalerts-activestorage-s3/main.tf
@@ -25,6 +25,27 @@ resource "aws_iam_user" "main" {
   name = "planningalerts-as-${var.env}"
 }
 
+resource "aws_iam_user_policy" "main" {
+  user   = aws_iam_user.main.name
+  policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action   = [
+            "s3:ListBucket",
+            "s3:PutObject",
+            "s3:GetObject",
+            "s3:DeleteObject",
+          ]
+          Effect   = "Allow"
+          Resource = "arn:aws:s3:::${aws_s3_bucket.main.id}/*"
+        },
+      ]
+      Version   = "2012-10-17"
+    }
+  )
+}
+
 resource "aws_iam_access_key" "main" {
   user = aws_iam_user.main.name
 }
