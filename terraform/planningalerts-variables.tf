90bf08e2e8b044dbfeeac26a46bbf9eed9ebd115
Matthew Landauer <mlandauer@gmail.com>
2023-04-20 12:09:14 +1000

    Reorder only

--- terraform/planningalerts-variables.tf
+++ terraform/planningalerts-variables.tf
@@ -1,23 +1,13 @@
+// BLUE environment
+
 variable "planningalerts_enable_blue_env" {
   description = "Enable planningalerts blue environment"
   type        = bool
   default     = true
 }
 
-variable "planningalerts_enable_green_env" {
-  description = "Enable planningalerts green environment"
-  type        = bool
-  default     = false
-}
-
 variable "planningalerts_blue_weight" {
   description = "Weighting of traffic to send to blue when enabled"
-  type        = number
-  default     = 1
-}
-
-variable "planningalerts_green_weight" {
-  description = "Weighting of traffic to send to green when enabled"
   type        = number
   default     = 1
 }
@@ -28,16 +18,30 @@ variable "planningalerts_blue_instance_count" {
   default     = 2
 }
 
-variable "planningalerts_green_instance_count" {
-  description = "Number of instance for green environment"
-  type        = number
-  default     = 2
-}
-
 variable "planningalerts_blue_ami" {
   # planningalerts-puma-ubuntu-22.04
   # TODO: Get this dynamically based on the name chosen in packer
   default = "ami-0574a77d61e67436d"
+}
+
+// GREEN environment
+
+variable "planningalerts_enable_green_env" {
+  description = "Enable planningalerts green environment"
+  type        = bool
+  default     = false
+}
+
+variable "planningalerts_green_weight" {
+  description = "Weighting of traffic to send to green when enabled"
+  type        = number
+  default     = 1
+}
+
+variable "planningalerts_green_instance_count" {
+  description = "Number of instance for green environment"
+  type        = number
+  default     = 2
 }
 
 variable "planningalerts_green_ami" {
