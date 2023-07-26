f816eaf00501e2f1d0e57910a1fca0aecdfa960f
Matthew Landauer <mlandauer@gmail.com>
2023-07-26 12:13:19 +1000

    Add rdns for cuttlefish

--- terraform/cuttlefish.tf
+++ terraform/cuttlefish.tf
@@ -10,3 +10,13 @@ resource "linode_instance" "cuttlefish" {
   # We want to set the "image" here but I don't think we can because
   # this was originally created with Ubuntu 14.04
 }
+
+resource "linode_rdns" "cuttlefish_ipv4" {
+  address = linode_instance.cuttlefish.ip_address
+  rdns    = "cuttlefish.oaf.org.au"
+}
+
+resource "linode_rdns" "cuttlefish_ipv6" {
+  address = cidrhost(linode_instance.cuttlefish.ipv6, 0)
+  rdns    = "cuttlefish.oaf.org.au"
+}
