50e372f72c51f06054d200687ddf5bebbc6a35c4
Matthew Landauer <mlandauer@gmail.com>
2024-08-28 10:03:14 +1000

    Move outputs

--- terraform/planningalerts/main.tf
+++ terraform/planningalerts/main.tf
@@ -65,26 +65,6 @@ resource "aws_iam_access_key" "sitemaps" {
   user = aws_iam_user.sitemaps.name
 }
 
-# TODO: These need to be output by the top level!
-
-# These values are needed by ansible for planningalerts
-# They should be encrypted and put in group_vars/planningalerts.yml
-# Take the output of this command:
-# terraform output planningalerts_sitemaps_production_access_key_id
-# cd ..; ansible-vault encrypt_string --name aws_access_key_id "value from above" --encrypt-vault-id default
-# AND
-# terraform output planningalerts_sitemaps_production_secret_access_key
-# cd ..; ansible-vault encrypt_string --name aws_secret_access_key "value from above" --encrypt-vault-id default
-
-output "planningalerts_sitemaps_production_secret_access_key" {
-  value     = aws_iam_access_key.sitemaps.secret
-  sensitive = true
-}
-
-output "planningalerts_sitemaps_production_access_key_id" {
-  value = aws_iam_access_key.sitemaps.id
-}
-
 resource "aws_iam_user_policy" "upload_to_sitemaps" {
   user = aws_iam_user.sitemaps.name
   name = "upload"
