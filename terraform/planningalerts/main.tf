697dda1331c9bd35ece550c87142bcb6289a4e41
Matthew Landauer <mlandauer@gmail.com>
2024-08-28 10:26:14 +1000

    Move sitemaps related stuff to its own file

--- terraform/planningalerts/main.tf
+++ terraform/planningalerts/main.tf
@@ -43,55 +43,6 @@ module "env-green" {
   zone_id              = var.zone_id
 }
 
-resource "aws_s3_bucket" "sitemaps" {
-  bucket = "planningalerts-sitemaps-production"
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "sitemaps" {
-  bucket = aws_s3_bucket.sitemaps.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_iam_user" "sitemaps" {
-  name = "planningalerts-sitemaps-production"
-}
-
-resource "aws_iam_access_key" "sitemaps" {
-  user = aws_iam_user.sitemaps.name
-}
-
-resource "aws_iam_user_policy" "upload_to_sitemaps" {
-  user = aws_iam_user.sitemaps.name
-  name = "upload"
-  policy = jsonencode(
-    {
-      Statement = [
-        {
-          Action = [
-            "s3:PutObject",
-            "s3:PutObjectAcl",
-          ]
-          Effect   = "Allow"
-          Resource = "arn:aws:s3:::${aws_s3_bucket.sitemaps.bucket}/*"
-        },
-      ]
-      Version = "2012-10-17"
-    }
-  )
-}
-
-module "activestorage-s3" {
-  source = "../planningalerts-activestorage-s3"
-
-  name            = "planningalerts-as-production"
-  allowed_origins = ["https://www.planningalerts.org.au"]
-}
-
 # TODO: Move this to its own file
 
 # In our setup we have a memcached server running alongside each webserver node
---
+++ terraform/planningalerts/sitemaps.tf
@@ -0,0 +1,48 @@
+resource "aws_s3_bucket" "sitemaps" {
+  bucket = "planningalerts-sitemaps-production"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sitemaps" {
+  bucket = aws_s3_bucket.sitemaps.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_iam_user" "sitemaps" {
+  name = "planningalerts-sitemaps-production"
+}
+
+resource "aws_iam_access_key" "sitemaps" {
+  user = aws_iam_user.sitemaps.name
+}
+
+resource "aws_iam_user_policy" "upload_to_sitemaps" {
+  user = aws_iam_user.sitemaps.name
+  name = "upload"
+  policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action = [
+            "s3:PutObject",
+            "s3:PutObjectAcl",
+          ]
+          Effect   = "Allow"
+          Resource = "arn:aws:s3:::${aws_s3_bucket.sitemaps.bucket}/*"
+        },
+      ]
+      Version = "2012-10-17"
+    }
+  )
+}
+
+module "activestorage-s3" {
+  source = "../planningalerts-activestorage-s3"
+
+  name            = "planningalerts-as-production"
+  allowed_origins = ["https://www.planningalerts.org.au"]
+}
