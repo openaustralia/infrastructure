2bb84a33cce149fbfb4a8d1c7d2dbd2e52008635
Matthew Landauer <mlandauer@gmail.com>
2024-08-29 10:47:57 +1000

    Renamed resources

--- terraform/righttoknow/dns.tf
+++ terraform/righttoknow/dns.tf
@@ -1,39 +1,55 @@
-variable "righttoknow_org_au_zone_id" {
-  default = "44b07a3486191276e3e6b0919dd86fff"
-}
-
 # A records
-resource "cloudflare_record" "rtk_root" {
+resource "cloudflare_record" "root" {
   zone_id = var.righttoknow_org_au_zone_id
   name    = "righttoknow.org.au"
   type    = "A"
-  value   = aws_eip.righttoknow.public_ip
+  value   = aws_eip.main.public_ip
+}
+
+moved {
+  from = cloudflare_record.rtk_root
+  to   = cloudflare_record.root
 }
 
 # CNAME records
-resource "cloudflare_record" "rtk_www" {
+resource "cloudflare_record" "www" {
   zone_id = var.righttoknow_org_au_zone_id
   name    = "www.righttoknow.org.au"
   type    = "CNAME"
   value   = "righttoknow.org.au"
 }
 
-resource "cloudflare_record" "rtk_test" {
+moved {
+  from = cloudflare_record.rtk_www
+  to   = cloudflare_record.www
+}
+
+resource "cloudflare_record" "test" {
   zone_id = var.righttoknow_org_au_zone_id
   name    = "test.righttoknow.org.au"
   type    = "CNAME"
   value   = "righttoknow.org.au"
 }
 
-resource "cloudflare_record" "rtk_www_test" {
+moved {
+  from = cloudflare_record.rtk_test
+  to   = cloudflare_record.test
+}
+
+resource "cloudflare_record" "www_test" {
   zone_id = var.righttoknow_org_au_zone_id
   name    = "www.test.righttoknow.org.au"
   type    = "CNAME"
   value   = "righttoknow.org.au"
 }
 
+moved {
+  from = cloudflare_record.rtk_www_test
+  to   = cloudflare_record.www_test
+}
+
 # MX records
-resource "cloudflare_record" "rtk_mx1" {
+resource "cloudflare_record" "mx1" {
   zone_id  = var.righttoknow_org_au_zone_id
   name     = "righttoknow.org.au"
   type     = "MX"
@@ -41,7 +57,12 @@ resource "cloudflare_record" "rtk_mx1" {
   value    = "aspmx.l.google.com"
 }
 
-resource "cloudflare_record" "rtk_mx2" {
+moved {
+  from = cloudflare_record.rtk_mx1
+  to   = cloudflare_record.mx1
+}
+
+resource "cloudflare_record" "mx2" {
   zone_id  = var.righttoknow_org_au_zone_id
   name     = "righttoknow.org.au"
   type     = "MX"
@@ -49,7 +70,12 @@ resource "cloudflare_record" "rtk_mx2" {
   value    = "alt1.aspmx.l.google.com"
 }
 
-resource "cloudflare_record" "rtk_mx3" {
+moved {
+  from = cloudflare_record.rtk_mx2
+  to   = cloudflare_record.mx2
+}
+
+resource "cloudflare_record" "mx3" {
   zone_id  = var.righttoknow_org_au_zone_id
   name     = "righttoknow.org.au"
   type     = "MX"
@@ -57,7 +83,12 @@ resource "cloudflare_record" "rtk_mx3" {
   value    = "alt2.aspmx.l.google.com"
 }
 
-resource "cloudflare_record" "rtk_mx4" {
+moved {
+  from = cloudflare_record.rtk_mx3
+  to   = cloudflare_record.mx3
+}
+
+resource "cloudflare_record" "mx4" {
   zone_id  = var.righttoknow_org_au_zone_id
   name     = "righttoknow.org.au"
   type     = "MX"
@@ -65,7 +96,12 @@ resource "cloudflare_record" "rtk_mx4" {
   value    = "aspmx2.googlemail.com"
 }
 
-resource "cloudflare_record" "rtk_mx5" {
+moved {
+  from = cloudflare_record.rtk_mx4
+  to   = cloudflare_record.mx4
+}
+
+resource "cloudflare_record" "mx5" {
   zone_id  = var.righttoknow_org_au_zone_id
   name     = "righttoknow.org.au"
   type     = "MX"
@@ -73,8 +109,13 @@ resource "cloudflare_record" "rtk_mx5" {
   value    = "aspmx3.googlemail.com"
 }
 
+moved {
+  from = cloudflare_record.rtk_mx5
+  to   = cloudflare_record.mx5
+}
+
 # TODO Check how this record is being used
-resource "cloudflare_record" "rtk_server" {
+resource "cloudflare_record" "server" {
   zone_id  = var.righttoknow_org_au_zone_id
   name     = "server.righttoknow.org.au"
   type     = "MX"
@@ -82,42 +123,72 @@ resource "cloudflare_record" "rtk_server" {
   value    = "righttoknow.org.au"
 }
 
+moved {
+  from = cloudflare_record.rtk_server
+  to   = cloudflare_record.server
+}
+
 # TXT records
-resource "cloudflare_record" "rtk_spf" {
+resource "cloudflare_record" "spf" {
   zone_id = var.righttoknow_org_au_zone_id
   name    = "righttoknow.org.au"
   type    = "TXT"
   value   = "v=spf1 a include:_spf.google.com ~all"
 }
 
-resource "cloudflare_record" "rtk_google_site_verification" {
+moved {
+  from = cloudflare_record.rtk_spf
+  to   = cloudflare_record.spf
+}
+
+resource "cloudflare_record" "google_site_verification" {
   zone_id = var.righttoknow_org_au_zone_id
   name    = "righttoknow.org.au"
   type    = "TXT"
   value   = "google-site-verification=ci77kXOm4-lxR3Tc1D1FlTzz0J_GWQES2wU5kFMIR-w"
 }
 
-resource "cloudflare_record" "rtk_facebook_domain_verification" {
+moved {
+  from = cloudflare_record.rtk_google_site_verification
+  to   = cloudflare_record.google_site_verification
+}
+
+resource "cloudflare_record" "facebook_domain_verification" {
   zone_id = var.righttoknow_org_au_zone_id
   name    = "righttoknow.org.au"
   type    = "TXT"
   value   = "facebook-domain-verification=vtlcbmfm4mihp4wql58lwz3nbhc8bt"
 }
 
+moved {
+  from = cloudflare_record.rtk_facebook_domain_verification
+  to   = cloudflare_record.facebook_domain_verification
+}
+
 # Note that this record comes from roles/internal/righttoknow/files/dkimkeys/default.txt which in turn is generated
 # by hand (as part of a keypair) using opendkim
-resource "cloudflare_record" "rtk_default_domainkey" {
+resource "cloudflare_record" "default_domainkey" {
   zone_id = var.righttoknow_org_au_zone_id
   name    = "default._domainkey.righttoknow.org.au"
   type    = "TXT"
   value   = "v=DKIM1; h=sha256; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzWFLO143fpHeVU27EjmOsY4hpKtR+/PI+idJItMWiHSwjgcX21QYMbQjPcHfHFhsDbblBBQy/MtcTwynSYFp1SWkTI8EsHLS1+pp1HAI3wx7ZWLmwE6di+qRKu+3ooPFSIUbA+TvA7GJmHBfBf/ubASWff4t5ByZ9edZOA4lZ7pGdG7O0duH+/hhggH/LFMPX6a0CzyXYjsfTvtyYMJvvRsoepEs/QjtdBarZS2roR7qxZQhSRUlbZgSNAbyO0+3wJptpxvAXleSuOFoN5nHMV4LT+vuF0g+FDxIpbJu+bW08IKL1qMSH8Gtwd20Hy34h88IHPg8zx5FUoeeOS5W/wIDAQAB"
 }
 
-resource "cloudflare_record" "rtk_google_domainkey" {
+moved {
+  from = cloudflare_record.rtk_default_domainkey
+  to   = cloudflare_record.default_domainkey
+}
+
+resource "cloudflare_record" "google_domainkey" {
   zone_id = var.righttoknow_org_au_zone_id
   name    = "google._domainkey.righttoknow.org.au"
   type    = "TXT"
   value   = "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAm30i+FaCipo1Eef8vrV66CRcdQGDfniuKP1ND2hj0VKiYf9LO15q7ZF9mE14zlOKmP//tS/EbdEXk6eAi0ps6oUf2jIvajyuDzLhz7Xn528LQDdxDRlh+2IdA+Z7jslLW7y0zJdYyp12X/Nx+mZrwbgoZJHplcmIZHQYWv00HX46ioR9eK8Yf6+0kU31ScAMcAphmjS4euYejsY0I0SoTlYDqJ/XNiiE2bl8wFfoG6/mgdHddpuPKKEs0cJc0Opt6ZzHuLdzQ+atnZJkqKQZWhkvrsMqeODBOoCE44SCW+5smT6TARDnGrnKTzvfEPZGoLQPojQHc3Ii+Bq3FtFsFwIDAQAB"
+}
+
+moved {
+  from = cloudflare_record.rtk_google_domainkey
+  to   = cloudflare_record.google_domainkey
 }
 
 # For the time being we're just using DMARC records to get some data on what's
@@ -126,9 +197,14 @@ resource "cloudflare_record" "rtk_google_domainkey" {
 # We're using a free service provided by https://dmarc.postmarkapp.com/
 # This generates a weekly DMARC report which gets sent by email on Monday mornings
 # Report goes to webmaster@righttoknow.org.au
-resource "cloudflare_record" "rtk_dmarc" {
+resource "cloudflare_record" "dmarc" {
   zone_id = var.righttoknow_org_au_zone_id
   name    = "_dmarc.righttoknow.org.au"
   type    = "TXT"
   value   = "v=DMARC1; p=none; pct=100; rua=mailto:re+aysyay6u9ct@dmarc.postmarkapp.com; sp=none; aspf=r;"
+}
+
+moved {
+  from = cloudflare_record.rtk_dmarc
+  to   = cloudflare_record.dmarc
 }
--- terraform/righttoknow/main.tf
+++ terraform/righttoknow/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 
-resource "aws_instance" "righttoknow" {
+resource "aws_instance" "main" {
   ami = var.ami
   # Changed it from t2.small to t2.medium because provisioning was very slow
   # Changed from t2.medium to t2.large because it was running out of memory
@@ -24,19 +24,29 @@ resource "aws_instance" "righttoknow" {
     var.security_group_webserver.name,
     var.security_group_incoming_email.name,
   ]
-  availability_zone       = aws_ebs_volume.righttoknow_data.availability_zone
+  availability_zone       = aws_ebs_volume.data.availability_zone
   disable_api_termination = true
   iam_instance_profile    = var.instance_profile.name
 }
 
-resource "aws_eip" "righttoknow" {
-  instance = aws_instance.righttoknow.id
+moved {
+  from = aws_instance.righttoknow
+  to   = aws_instance.main
+}
+
+resource "aws_eip" "main" {
+  instance = aws_instance.main.id
   tags = {
     Name = "righttoknow"
   }
 }
 
-resource "aws_ebs_volume" "righttoknow_data" {
+moved {
+  from = aws_eip.righttoknow
+  to   = aws_eip.main
+}
+
+resource "aws_ebs_volume" "data" {
   availability_zone = "ap-southeast-2c"
 
   size = 240
@@ -46,8 +56,18 @@ resource "aws_ebs_volume" "righttoknow_data" {
   }
 }
 
-resource "aws_volume_attachment" "righttoknow_data" {
+moved {
+  from = aws_ebs_volume.righttoknow_data
+  to   = aws_ebs_volume.data
+}
+
+resource "aws_volume_attachment" "data" {
   device_name = "/dev/sdi"
-  volume_id   = aws_ebs_volume.righttoknow_data.id
-  instance_id = aws_instance.righttoknow.id
+  volume_id   = aws_ebs_volume.data.id
+  instance_id = aws_instance.main.id
+}
+
+moved {
+  from = aws_volume_attachment.righttoknow_data
+  to   = aws_volume_attachment.data
 }
