0cf470ecf787c1b24dd2909f85ae03104783cfc6
Matthew Landauer <mlandauer@gmail.com>
2024-09-09 10:12:14 +1000

    Rename resources

--- terraform/campaign-monitor/dns.tf
+++ terraform/campaign-monitor/dns.tf
@@ -6,16 +6,26 @@ terraform {
     }
   }
 }
-resource "cloudflare_record" "campaign_monitor_root" {
+resource "cloudflare_record" "root" {
   zone_id = var.zone_id
   name    = "email.oaf.org.au"
   type    = "CNAME"
   value   = "cname.createsend.com"
 }
 
-resource "cloudflare_record" "campaign_monitor_domainkey" {
+moved {
+  from = cloudflare_record.campaign_monitor_root
+  to   = cloudflare_record.root
+}
+
+resource "cloudflare_record" "domainkey" {
   zone_id = var.zone_id
   name    = "cm._domainkey.oaf.org.au"
   type    = "TXT"
   value   = "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC7c0O/Ihi0wMb89k9UvkFPqM00DWEcm5kgCkhSTHN5rKcMtlCrijBYqZQgBcig/M6Zl6o6z9nKp4egpJ9Yf8ndZEz/r7AcQIeTjLwxIIlFSbABuBoQPoxTUrIvzRCWUTgCocvi3sNrzxYvYfFPq7LmxjI+RzK3UD84rKBaJtYULwIDAQAB"
+}
+
+moved {
+  from = cloudflare_record.campaign_monitor_domainkey
+  to   = cloudflare_record.domainkey
 }
