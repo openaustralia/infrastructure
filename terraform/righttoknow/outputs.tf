ec0a94e2ea55ec5774de58e5fb9276f54e980ff2
Ben Fairless <ben@oaf.org.au>
2025-10-23 07:59:52 +0800

    Add new Ubuntu 22.04 production server configuration for Right to Know in Terraform

--- terraform/righttoknow/outputs.tf
+++ terraform/righttoknow/outputs.tf
@@ -13,3 +13,19 @@ output "staging_dns" {
   value       = "staging.righttoknow.org.au"
   description = "Main DNS name for staging"
 }
+
+# Output production server details for use with Ansible
+output "production_public_ip" {
+  value       = aws_eip.production.public_ip
+  description = "Public IP address of the production server"
+}
+
+output "production_instance_id" {
+  value       = aws_instance.production.id
+  description = "Instance ID of the production server"
+}
+
+output "production_dns" {
+  value       = "righttoknow.org.au"
+  description = "Main DNS name for production"
+}
