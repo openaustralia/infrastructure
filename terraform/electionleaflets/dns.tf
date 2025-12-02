87297f68dcbf56d2729c2cad1d69ee00c484e09d
Brenda Wallace <brenda@wallace.net.nz>
2025-12-02 17:52:42 +1300

    keep the electionleaflets domain

---
+++ terraform/electionleaflets/dns.tf
@@ -0,0 +1,22 @@
+resource "cloudflare_zone" "main" {
+  account_id = var.cloudflare_account_id
+  plan       = "free"
+  zone       = "electionleaflets.org.au"
+}
+
+# A records
+resource "cloudflare_record" "root" {
+  zone_id = cloudflare_zone.main.id
+  name    = "electionleaflets.org.au"
+  type    = "A"
+  value   = aws_eip.electionleaflets.public_ip
+}
+
+# TODO: Can we extract the creation of several cnames into a simple module?
+# CNAME records
+resource "cloudflare_record" "www" {
+  zone_id = cloudflare_zone.main.id
+  name    = "www.electionleaflets.org.au"
+  type    = "CNAME"
+  value   = "electionleaflets.org.au"
+}
