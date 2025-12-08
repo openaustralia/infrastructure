4877570ea09631ac929400aaf2faf14b00be57cd
Brenda Wallace <brenda@wallace.net.nz>
2025-12-09 12:32:53 +1300

    less fluff

--- scripts/get_rds_pass.sh
+++ scripts/get_rds_pass.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 
-p=$(ansible-playbook ./scripts/show_secrets.yml | \
-    grep "RDS Admin Password:" | \
-    sed 's/.*RDS Admin Password://' |\
-    sed 's/\\nBackup Target Pass: .*$//' )
-
-
-echo ${p}
-
+ansible-playbook ./scripts/show_secrets.yml | \
+    sed -n 's/.*Password: \(.*\)/\1/p' |
+    head -n 1
