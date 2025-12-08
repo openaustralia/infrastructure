ae97b3f81b458844d49861a0adcac3fd3a9f5aab
Brenda Wallace <brenda@wallace.net.nz>
2025-12-09 12:40:06 +1300

    Add gzip compression for MySQL dumps and improve output message

--- scripts/mysql-dump.sh
+++ scripts/mysql-dump.sh
@@ -16,3 +16,8 @@ pass=$(./scripts/get_rds_pass.sh)
 echo "Dumping database '$1' to 'db_dumps/$output'..."
 mysqldump "$1" --host=127.0.0.1 --port=3306 --user=admin --password="${pass}" \
     > "db_dumps/$output"
+
+cd db_dumps
+gzip "$output"
+
+echo "Database dump completed: db_dumps/$output.gz"
