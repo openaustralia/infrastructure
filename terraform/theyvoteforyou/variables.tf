96fb2ee84f5392fd30eb49b933302dc1021ef77f
Ben Fairless <ben@oaf.org.au>
2026-03-16 14:43:31 +0800

    Remove duplicate webserver security group variable from theyvoteforyou

--- terraform/theyvoteforyou/variables.tf
+++ terraform/theyvoteforyou/variables.tf
@@ -1,6 +1,5 @@
 variable "ami" {}
 variable "deployer_key" {}
-variable "security_group" {}
 variable "security_group_service" {}
 variable "instance_profile" {}
 variable "cloudflare_account_id" {}
