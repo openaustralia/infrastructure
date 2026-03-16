c9ff9192377962fcd9d64115d7bb5e16e6ac26e0
Ben Fairless <ben@oaf.org.au>
2026-03-16 14:44:30 +0800

    chore: remove webserver security group references from righttoknow

--- terraform/righttoknow/staging.tf
+++ terraform/righttoknow/staging.tf
@@ -18,7 +18,6 @@ resource "aws_instance" "staging" {
   }
 
   vpc_security_group_ids = [
-    var.security_group_webserver.id,
     var.security_group_service.id,
     var.security_group_incoming_email.id,
   ]
--- terraform/righttoknow/variables.tf
+++ terraform/righttoknow/variables.tf
@@ -1,4 +1,3 @@
-variable "security_group_webserver" {}
 variable "security_group_service" {}
 variable "security_group_incoming_email" {}
 variable "instance_profile" {}
