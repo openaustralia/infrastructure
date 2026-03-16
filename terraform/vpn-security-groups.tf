d9dab307e8bfe42fbf18fcb01925b30278684d79
Ben Fairless <ben@oaf.org.au>
2026-03-16 14:49:26 +0800

    chore: remove webserver security group SSH rule

--- terraform/vpn-security-groups.tf
+++ terraform/vpn-security-groups.tf
@@ -43,18 +43,6 @@ locals {
   vpn_subnet = "10.8.0.0/24"
 }
 
-# Update webserver security group to allow SSH only from VPN
-# This replaces the existing SSH rule that allows 0.0.0.0/0
-resource "aws_security_group_rule" "webserver_ssh_from_vpn" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = [local.vpn_subnet]
-  security_group_id = aws_security_group.webserver.id
-  description       = "SSH from VPN clients only"
-}
-
 # Update planningalerts security group to allow SSH only from VPN
 resource "aws_security_group_rule" "planningalerts_ssh_from_vpn" {
   type              = "ingress"
